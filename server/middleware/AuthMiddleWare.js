96558ddb4528acfc3b32388bf0fe813e67f86e15
Oscar <70218472+oscarpergler@users.noreply.github.com>
2024-02-09 12:39:25 +0100

    updated auth-middleware

--- server/middleware/AuthMiddleWare.js
+++ server/middleware/AuthMiddleWare.js
@@ -1,49 +1,22 @@
-const User = require("../models/UserModel");
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-const authRefresh = async (dataID)  => {
-  const user = await User.findById(dataID)
-  jwt.verify(user.token, process.env.REFRESHTOKEN_KEY, async (err, data) => {
-    if (err){ // Refresh token has expired => user needs to re-authorize
-      return false;
-    } else{ // Refresh token is valid => refresh access token
-      res.cookie("token", generateAccessToken(user), {
-        withCredentials: true,
-        httpOnly: true,
-      });
-      return true;
+const userVerification = (req, res, next) => {
+  const header = req.headers['authorization'];
+  if (!header) return res.sendStatus(401);
+  const token = header.split(' ')[1] // Since the header will contain: "Bearer [actual token value]"
+  jwt.verify(
+    token, 
+    process.env.ACCESSTOKEN_KEY, 
+    async (err, data) => {
+      if (err) {
+        return res.sendStatus(403); // invalid token
+      } else {
+        req.user = data.username;
+        next();
+      }
     }
-  })
-}
-
-const generateAccessToken = (user) => {
-  jwt.sign(
-    {"id": user.id, "email": user.email, "username": user.username, "role": user.role},
-    process.env.ACCESSTOKEN_KEY,
-    { expiresIn: '10m' }
   )
-} 
-
-// https://youtu.be/favjC6EKFgw?si=6Nm9_ePs45ybscc_&t=1156
-
-module.exports.userVerification = (req, res) => {
-  const token = req.cookies.token
-  if (!token) {
-    return res.json({ status: false })
-  }
-  jwt.verify(token, process.env.ACCESSTOKEN_KEY, async (err, data) => {
-    if (err) {
-      if (data){
-        if (authRefresh(data.id)){
-          return res.json({ status: true, user: data.username })
-        }
-      } 
-      return res.json({ status: false })
-    } else {
-      const user = await User.findById(data.id)
-      if (user) return res.json({ status: true, user: user.username })
-      else return res.json({ status: false })
-    }
-  })
 }
+
+module.exports = userVerification;
--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,8 +1,6 @@
 const { Signup, Login } = require("../controllers/AuthController");
-const { userVerification } = require("../middleware/AuthMiddleWare");
 const router = require("express").Router();
 
-router.post('/', userVerification)
 router.post('/signup', Signup)
 router.post('/login', Login)
 
